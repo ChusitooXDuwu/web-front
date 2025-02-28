08fbdab0fef7d0257913faeef9847dcac6f4c991
ChusitooXDuwu <w.chu@uniandes.edu.co>
2025-02-28 18:47:47 +0000

    changes for hot loading in webpack

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Te amo duolingo</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edittt <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
