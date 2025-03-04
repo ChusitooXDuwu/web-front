730e733d6fd9f60f7d916abc045e71c354c37356
Danielfts <d.trivino@uniandes.edu.co>
2025-03-04 18:10:38 +0000

    Feature: Added colors

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -9,45 +9,45 @@ import { Link } from "react-router-dom";
 
 function LoginPage() {
   return (
-      <Container className={styles.login_container}>
-        <Col>
-          <Row>
-            <h1 className={`display-1 ${styles.sporthub_title}`}>SportHub</h1>
-          </Row>
-          <Row>
-            <Card className={styles.login_card}>
-              <Card.Body>
-                <Form>
-                  <Form.Group>
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control
-                      type="email"
-                      placeholder="Ingrese su correo"
-                    />
-                  </Form.Group>
-                  <Form.Group>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                      type="password"
-                      placeholder="Ingrese su contraseña"
-                    />
-                  </Form.Group>
-                </Form>
-              </Card.Body>
-            </Card>
-          </Row>
-          <Row>
-            <Button className={styles.login_button} variant="primary">
-              Login
-            </Button>
-          </Row>
-          <Row>
-            <span className={styles.register_text}>
-              ¿Nuevo en Sporthub? <Link to={"/signup"}>Registrese aquí</Link>
-            </span>
-          </Row>
-        </Col>
-      </Container>
+    <Container className={styles.login_container}>
+      <Col>
+        <Row>
+          <h1 className={`display-1 ${styles.sporthub_title}`}>SportHub</h1>
+        </Row>
+        <Row>
+          <Card className={styles.login_card}>
+            <Card.Body>
+              <Form>
+                <Form.Group>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" placeholder="Ingrese su correo" />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Ingrese su contraseña"
+                  />
+                </Form.Group>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Row>
+        <Row>
+          <Button className={styles.login_button} variant="primary">
+            Login
+          </Button>
+        </Row>
+        <Row>
+          <p className={styles.register_text}>
+            ¿Nuevo en Sporthub?{" "}
+            <Link className={styles.register_link} to={"/signup"}>
+              Registrese aquí
+            </Link>
+          </p>
+        </Row>
+      </Col>
+    </Container>
   );
 }
 
