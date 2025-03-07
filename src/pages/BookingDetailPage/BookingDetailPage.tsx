2e79bb5499763e3c2ce6290b377e038ffb594f72
Danielfts <d.trivino@uniandes.edu.co>
2025-03-07 14:24:31 +0000

    Featue: completed scaffolding

---
+++ src/pages/BookingDetailPage/BookingDetailPage.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import styles from './BookingDetailPage.module.scss';
+
+interface BookingDetailPageProps {}
+
+const BookingDetailPage: FC<BookingDetailPageProps> = () => (
+  <div className={styles.BookingDetailPage}>
+    BookingDetailPage Component
+  </div>
+);
+
+export default BookingDetailPage;
---
+++ src/pages/FieldDetailPage/FieldDetailPage.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import styles from './FieldDetailPage.module.scss';
+
+interface FieldDetailPageProps {}
+
+const FieldDetailPage: FC<FieldDetailPageProps> = () => (
+  <div className={styles.FieldDetailPage}>
+    FieldDetailPage Component
+  </div>
+);
+
+export default FieldDetailPage;
