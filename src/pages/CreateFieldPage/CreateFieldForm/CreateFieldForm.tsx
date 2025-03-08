276c056e4a199035040a8925e09e4d98e8b74609
Danielfts <d.trivino@uniandes.edu.co>
2025-03-08 16:21:18 +0000

    Feature: Completed form reactivity

--- src/pages/CreateFieldPage/CreateFieldForm/CreateFieldForm.tsx
+++ src/pages/CreateFieldPage/CreateFieldForm/CreateFieldForm.tsx
@@ -1,30 +1,47 @@
-import { FunctionComponent, useState } from "react";
-import { Button, Form, FormControl, FormLabel } from "react-bootstrap";
+import React, { ChangeEvent, FunctionComponent, useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { SportEntity, CityEntity } from "../../../entities/Entities";
+import styles from "./CreateFieldFrom.module.scss";
 
 interface CreateFieldFormData {
   name: string;
-  city: string;
+  cityId: string;
   address: string;
-  sports: string[];
+  sportIds: string[];
   price: number;
 }
 
 interface CreateFieldFormProps {
-  cities: any[];
+  cities: CityEntity[];
+  sports: SportEntity[];
 }
 
 const CreateFieldForm: FunctionComponent<CreateFieldFormProps> = ({
   cities,
+  sports,
 }) => {
   const [formData, setFormData] = useState<CreateFieldFormData>({
     name: "",
-    city: "",
+    cityId: "",
     address: "",
-    sports: [],
+    sportIds: [],
     price: 0,
   });
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleCheckBoxChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value, checked } = e.target;
+    const updatedCheckedSports = checked
+      ? [...formData.sportIds, value]
+      : formData.sportIds.filter((item) => item !== value);
+    setFormData({ ...formData, sportIds: updatedCheckedSports });
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -32,35 +49,67 @@ const CreateFieldForm: FunctionComponent<CreateFieldFormProps> = ({
     console.log("Form submitted:", formData);
   };
   return (
-    <Form  onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit}>
       <Form.Group>
-        <FormLabel>Nombre</FormLabel>
-        <FormControl name="name" onChange={handleChange} placeholder="Ingrese el nombre del establecimiento" />
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control
+          name="name"
+          onChange={handleChange}
+          placeholder="Ingrese el nombre del establecimiento"
+          required
+        />
       </Form.Group>
       <Form.Group>
-        <FormLabel>Ciudad</FormLabel>
-        <Form.Select name="city">
+        <Form.Label>Ciudad</Form.Label>
+        <Form.Select name="cityId" onChange={handleChange} required>
+          <option value={""}>Seleccione una opción</option>
           {cities.map((item, index) => (
-            <option key={index} value={item}>{item}</option>
+            <option key={index} value={item.id}>
+              {item.name}
+            </option>
           ))}
         </Form.Select>
       </Form.Group>
       <Form.Group>
-        <FormLabel>Dirección</FormLabel>
-        <FormControl name="address" onChange={handleChange} placeholder="Ingrese la dirección" />
+        <Form.Label>Dirección</Form.Label>
+        <Form.Control
+          name="address"
+          onChange={handleChange}
+          placeholder="Ingrese la dirección"
+          required
+        />
       </Form.Group>
       <Form.Group>
-        <FormLabel>Precio</FormLabel>
-        <FormControl name="price" onChange={handleChange} placeholder="Ingrese el precio" />
+        <Form.Label>Precio</Form.Label>
+        <Form.Control
+          name="price"
+          type="number"
+          min={0}
+          onChange={handleChange}
+          placeholder="Ingrese el precio"
+          required
+        />
       </Form.Group>
       <Form.Group>
-        <FormLabel>Deportes</FormLabel>
-        <FormControl name="" onChange={handleChange} placeholder="Seleccione un deporte" />
+        <Form.Label>Deportes</Form.Label>
+        <Row>
+          {sports.map((item, index) => (
+            <Col key={index} className={styles.check_col}>
+              <Form.Check
+                type="checkbox"
+                label={item.name}
+                value={item.id}
+                onChange={handleCheckBoxChange}
+              />
+            </Col>
+          ))}
+        </Row>
       </Form.Group>
       <Form.Group>
-        <FormLabel>Fotos del establecimiento</FormLabel>
-        <FormControl onChange={handleChange} type="file" multiple />
+        <Form.Label>Fotos del establecimiento</Form.Label>
+        <Form.Control type="file" multiple />
       </Form.Group>
+      <pre>{JSON.stringify(formData, null, 2)}</pre>
       <div className="d-flex justify-content-center">
         <Button type="submit" className="mt-3">
           Crear Cancha
