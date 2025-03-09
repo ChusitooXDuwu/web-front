5bd267e04f7ba5fe3e79352ce32160c76a9c184b
Danielfts <d.trivino@uniandes.edu.co>
2025-03-09 16:29:20 +0000

    Feature: Added navigation to create page

--- src/pages/FieldsPage/FieldsPage.tsx
+++ src/pages/FieldsPage/FieldsPage.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import styles from "./FieldsPage.module.scss";
 import { Breadcrumb, Button, Col, Container, Form, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { CityEntity, FieldEntity, SportEntity } from "../../entities/Entities";
 import FieldCardComponent from "./FieldCardComponent/FieldCardComponent";
 
@@ -11,22 +11,19 @@ const FieldsPage: FC<FieldsPageProps> = () => {
   const mockCity: CityEntity = {
     id: "1",
     name: "Mock City",
-    // other fields if any...
   };
-  
+
   const mockSports: SportEntity[] = [
     {
       id: "1",
       name: "Soccer",
-      // other fields if any...
     },
     {
       id: "2",
       name: "Basketball",
-      // other fields if any...
     },
   ];
-  
+
   const mockFieldEntity: FieldEntity = {
     id: "1",
     name: "Mock Field",
@@ -36,6 +33,8 @@ const FieldsPage: FC<FieldsPageProps> = () => {
     createdById: "user123",
     // other fields from BaseEntity if any...
   };
+  // Hooks
+  const navigate = useNavigate();
   const FieldsData: Array<FieldEntity> = Array(10).fill(mockFieldEntity);
   return (
     <div className="main_content_container pt-2">
@@ -46,16 +45,20 @@ const FieldsPage: FC<FieldsPageProps> = () => {
           </Breadcrumb.Item>
           <Breadcrumb.Item active>Canchas</Breadcrumb.Item>
         </Breadcrumb>
-        <Row xs={1} md={2} className="mb-3">
+        <Row className="mb-3 gy-2">
           <Col>
             <h1 className={`display-5 sh_gold`}>Canchas disponibles</h1>
           </Col>
-          <Col className={styles.search_col}>
+          <Col md="auto" className={styles.create_col}>
+            <Button onClick={() => navigate("/fields/create")}>
+              Crear Cancha
+            </Button>
+          </Col>
+          <Col md="auto" className={styles.search_col}>
             <Form className={styles.search_form}>
               <Form.Control
                 type="search"
                 placeholder="Buscar"
-                className="me-2"
                 aria-label="Search"
               />
               <Button variant="outline-success">Search</Button>
@@ -65,7 +68,7 @@ const FieldsPage: FC<FieldsPageProps> = () => {
         <Row lg={3} md={3} sm={2} xs={1} className="gy-3">
           {FieldsData.map((item, index) => (
             <Col key={index}>
-              <FieldCardComponent fieldData={item}/>
+              <FieldCardComponent fieldData={item} />
             </Col>
           ))}
         </Row>
