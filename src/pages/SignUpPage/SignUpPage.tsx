efdb92fef2e6e2341f088af6e50b2a4bd3179474
Danielfts <d.trivino@uniandes.edu.co>
2025-03-09 18:56:36 +0000

    Feature: Added sign up form handling

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,16 +1,44 @@
-import { FunctionComponent } from "react";
+import { ChangeEvent, FormEvent, FunctionComponent, useState } from "react";
 import styles from "./SignUpPage.module.scss";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Card from "react-bootstrap/esm/Card";
 import Form from "react-bootstrap/esm/Form";
 import Button from "react-bootstrap/esm/Button";
 
+interface SignUpData {
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  birthDate: Date;
+}
+
 interface SignUpPageProps {}
 
 const SignUpPage: FunctionComponent<SignUpPageProps> = () => {
+  const navigate = useNavigate();
+  const [signUpData, setSignUpData] = useState<SignUpData>({
+    name: "",
+    email: "",
+    password: "",
+    phone: "",
+    birthDate: new Date(),
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setSignUpData({ ...signUpData, [name]: value });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted", signUpData);
+    navigate("/login");
+  };
+
   return (
     <Container fluid className={"px-md-5 py-2"}>
       <Row>
@@ -31,35 +59,63 @@ const SignUpPage: FunctionComponent<SignUpPageProps> = () => {
       <Row>
         <Card className={styles.signup_card}>
           <Card.Body>
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <Form.Group>
                 <Form.Label>Nombre</Form.Label>
-                <Form.Control type="text" placeholder="Ingrese su nombre" />
+                <Form.Control
+                  required
+                  name="name"
+                  onChange={handleChange}
+                  type="text"
+                  placeholder="Ingrese su nombre"
+                />
               </Form.Group>
               <Form.Group>
                 <Form.Label>Correo</Form.Label>
-                <Form.Control type="email" placeholder="Ingrese su correo" />
+                <Form.Control
+                  required
+                  name="email"
+                  onChange={handleChange}
+                  type="email"
+                  placeholder="Ingrese su correo"
+                />
               </Form.Group>
               <Form.Group>
                 <Form.Label>Contraseña</Form.Label>
                 <Form.Control
+                  required
+                  name="password"
+                  onChange={handleChange}
                   type="password"
                   placeholder="Ingrese su contraseña"
                 />
               </Form.Group>
               <Form.Group>
                 <Form.Label>Teléfono</Form.Label>
-                <Form.Control type="tel" placeholder="Ingrese su teléfono" />
+                <Form.Control
+                  required
+                  name="phone"
+                  onChange={handleChange}
+                  type="tel"
+                  placeholder="Ingrese su teléfono"
+                />
               </Form.Group>
               <Form.Group>
                 <Form.Label>Fecha de Nacimiento</Form.Label>
-                <Form.Control type="date" />
+                <Form.Control
+                  required
+                  name="birthDate"
+                  onChange={handleChange}
+                  type="date"
+                />
               </Form.Group>
+              <br />
+              <Row>
+                <Button type="submit" className={styles.submit_button}>
+                  Crear Cuenta
+                </Button>
+              </Row>
             </Form>
-            <br />
-            <Row>
-              <Button className={styles.submit_button}>Crear Cuenta</Button>
-            </Row>
           </Card.Body>
         </Card>
       </Row>
