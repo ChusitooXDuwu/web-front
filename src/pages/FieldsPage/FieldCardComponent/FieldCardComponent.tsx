098d828a7ff1985c3fa2c1548649eed19a30e304
Danielfts <d.trivino@uniandes.edu.co>
2025-03-09 03:16:18 +0000

    Feature: Completed basic styling of cards

--- src/pages/FieldsPage/FieldCardComponent/FieldCardComponent.tsx
+++ src/pages/FieldsPage/FieldCardComponent/FieldCardComponent.tsx
@@ -6,22 +6,14 @@ interface FieldCardComponentProps {}
 
 const FieldCardComponent: FunctionComponent<FieldCardComponentProps> = () => {
   return (
-    <Card className={`bg-dark text-white ${styles.field_card}`}>
-      <Card.Body className="p-0">
-        <Card.Img
-          src="assets/stock-bb-court.jpeg"
-          alt="Card image"
-          className={styles.field_card_img}
-          // style={{ filter: "brightness(50%)" }}
-        />
-        <Card.ImgOverlay>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </Card.Text>
-          <Card.Text>Last updated 3 mins ago</Card.Text>
-        </Card.ImgOverlay>
+    <Card className={`text-white ${styles.field_card}`}>
+      <Card.Body className={styles.card_body}>
+        <Card.Title>Card title</Card.Title>
+        <Card.Text>
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </Card.Text>
+        <Card.Text>Last updated 3 mins ago</Card.Text>
       </Card.Body>
     </Card>
   );
