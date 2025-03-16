ea5a062fc0475ed89f20705ef4c0cddf516118dd
Danielfts <d.trivino@uniandes.edu.co>
2025-03-16 05:21:08 +0000

    Feature:  completed signup intl

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -4,8 +4,20 @@ const es: { [key: string]: string } = {
   password: "Password",
   passwordPlaceholder: "Enter your password",
   login: "Log In",
+  signup: "Sign Up",
   newInSporthub: "New to sporthub?",
   registerHere: "Register Here",
+  areYouAMember: "Are you a sporthub member?",
+  "form.placeholder.name": "Enter your name",
+  "form.placeholder.email": "Enter your email",
+  "form.placeholder.password": "Enter your password",
+  "form.placeholder.phone": "Enter your phone",
+  "form.placeholder.birthdate": "Enter your birthdate",
+  "form.label.name": "Name",
+  "form.label.email": "Email",
+  "form.label.password": "Password",
+  "form.label.phone": "Phone",
+  "form.label.birthdate": "Birth date",
 };
 
 export default es;
--- src/locales/es.ts
+++ src/locales/es.ts
@@ -4,8 +4,20 @@ const en: { [key: string]: string } = {
   password: "Contraseña",
   passwordPlaceholder: "Ingrese su contraseña",
   login: "Ingresar",
+  signup: "Registrarse",
   newInSporthub: "¿Nuevo en Sporthub?",
   registerHere: "Registrese Aquí",
+  areYouAMember: "¿Eres miembro de sporthub?",
+  "form.placeholder.name": "Ingrese su nombre",
+  "form.placeholder.email": "Ingrese su correo",
+  "form.placeholder.password": "Ingrese su contraseña",
+  "form.placeholder.phone": "Ingrese su telefono",
+  "form.placeholder.birthdate": "Ingrese su fecha de nacimiento",
+  "form.label.name": "Nombre",
+  "form.label.email": "Correo",
+  "form.label.password": "Contraseña",
+  "form.label.phone": "Telefono",
+  "form.label.birthdate": "Fecha de nacimiento",
 };
 
 export default en;
